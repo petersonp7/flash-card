e69b8a297e9c81665c387c9725f62dbe199c0951
petersonp7 <149385281+petersonp7@users.noreply.github.com>
2024-10-07 08:00:34 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'IA',
+    'contribuiçoes relevantes',
+    'assistentes virtuais, diagnostico medico, carros autonomos'
+)
+
+criaCartao(
+    'IA',
+    'curiosidades',
+    'o conceito IA existe desde os anos 1950, IA e utilizada em jogos para criar oponentes mais inteligentes'
+)
+
+criaCartao(
+    'IA',
+    'fatos importantes',
+    'IA pode mudar para sempre o mercado de trabalho'
+)
+
+criaCartao(
+    'IA',
+    'tipos de IA',
+    'Os principais tipos de inteligência artificial são: Inteligência Artificial Limita (ANI), que é programada para armazenar grande volume de dados; Inteligência artificial geral (AGI), como machine learning e a capacidade de aprender e a reagir estímulos como um humano; e Superinteligência (ASI)'
+)
